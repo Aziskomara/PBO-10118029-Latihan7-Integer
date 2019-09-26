9d400344b6d4c77393208087991fd862f64b1cd5
Aziskomara <55784203+Aziskomara@users.noreply.github.com>
2019-09-26 15:30:13 +0700

    Add files via upload

---
+++ Latihan7.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan7;
+
+/**
+ * Nama     : Azis komara
+ * Kelas    : IF-1
+ * Nim      : 10118029
+ * deskiripsi program   : Nilai integer
+ * 
+ * @author Azis KOmara
+ */
+public class Latihan7 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        byte b;
+        short s;
+        int i;
+        long l;
+        b = 120;
+        s = 32767;
+        i = 1_000_000_000;
+        l = 90000000000L;
+        System.out.println("byte : " + b);
+        System.out.println("short : " + s);
+        System.out.println("int : " + i);
+        System.out.println("long : " + l);
+        
+    }
+    
+}
